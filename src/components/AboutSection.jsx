d25243fb251f9e3a408bd3c1fc4b4b015551ceaf
Joel Barbosa <joelbarbosa@gmail.com>
2021-01-14 19:37:07 +0100

    About Us animation

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,6 +7,8 @@ import { BasicLayout, Description, Image, Hide } from "../styles";
 
 //Framer Motion
 import { motion } from "framer-motion";
+import { titleAnimation, fadeAnimation, photoAnimation } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -14,26 +16,31 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We Work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We Work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fadeAnimation}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fadeAnimation}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="guy with a camera"
+        />
       </Image>
+      <Wave />
     </About>
   );
 };
--- src/styles.js
+++ src/styles.js
@@ -12,6 +12,7 @@ export const BasicLayout = styled.div`
 export const Description = styled.div`
   flex: 1;
   padding-right: 5rem;
+  z-index: 2;
   h2 {
     font-weight: lighter;
   }
@@ -20,6 +21,8 @@ export const Description = styled.div`
 export const Image = styled.div`
   flex: 1;
   overflow: hidden;
+  z-index: 2;
+
   img {
     width: 100%;
     height: 80vh;

67712137f74bc47be2020f21713dfb517e91b183
Joel Barbosa <joelbarbosa@gmail.com>
2021-01-14 01:26:12 +0100

    About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+//Pages
+import AboutUs from "./pages/AboutUs";
+
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//Components
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;

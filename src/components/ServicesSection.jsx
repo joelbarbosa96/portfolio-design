02d282353fd22f480e3c3a75c03898ef6225bf57
Joel Barbosa <joelbarbosa@gmail.com>
2021-01-14 01:42:13 +0100

    Services and FAQ section.

---
+++ src/components/ServicesSection.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+//Icons
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+
+//Images
+import home2 from "../img/home2.png";
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          High <span>quality</span> services.
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} alt="Clock icon" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="Teamwork Icon" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={diaphragm} alt="Diaphragm icon" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="Money Icon" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={home2} alt="Camera" />
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
